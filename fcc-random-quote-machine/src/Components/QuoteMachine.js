cacc2afa4d15d70f312e66075ca2a5b1aff24dda
brandon quezada <brandon.cypaso@gmail.com>
2020-05-12 12:59:42 -0500

    puting random quote machine in new repo

---
+++ fcc-random-quote-machine/src/Components/QuoteMachine.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const QuoteMachine = ({ assignNewQuoteIndex, selectedQuote }) => (
+	<Card>
+		<CardContent>
+			<Typography id='text'>
+				{selectedQuote.quote} - <span id='author'>{selectedQuote.author}</span>
+			</Typography>
+
+			<CardActions>
+				<Button
+					variant='outlined'
+					color='primary'
+					className='button'
+					id='new-quote'
+					size='small'
+					onClick={assignNewQuoteIndex}
+				>
+					Next Quote
+				</Button>
+				<IconButton
+					variant='outlined'
+					color='primary'
+					id='tweet-quote'
+					target='_blank'
+					href={encodeURI(
+						`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=ThePowerOfInspiration`
+					)}
+				>
+					<FontAwesomeIcon icon={faTwitter} size='sm'></FontAwesomeIcon>
+				</IconButton>
+			</CardActions>
+		</CardContent>
+	</Card>
+);
+
+export default QuoteMachine;
